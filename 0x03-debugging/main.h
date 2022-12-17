76caf39a887b45ccd8a030325dc726061c37c2e5
AKINTEMTJ <akinyemitjtemitope@gmail.com>
2022-12-17 07:43:48 -0800

    1-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -8,6 +8,5 @@ int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 void print_time_table(int n);
 int largest_number(int a, int b, int c);
-int main(void);
 
 #endif
