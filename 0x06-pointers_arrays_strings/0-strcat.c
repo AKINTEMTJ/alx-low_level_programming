bc06108635d7d11a2d284c2481d3f020408edce8
AKINTEMTJ <akinyemitjtemitope@gmail.com>
2022-12-23 09:07:12 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * _strncat - function that concatenates two strings
+ * _strcat - function that concatenates two strings
  * @dest: char
  * @src: char
  * Return: char
  */
+
 char *_strcat(char *dest, char *src)
 {
 	char *s = dest;
